30012b7976650765b909d373a8e29e03fdd7cd9d
parismollo <parismollochristondis@gmail.com>
2022-05-01 23:07:10 +0200

    exercice 6 - not good.

--- Makefile
+++ Makefile
@@ -3,23 +3,20 @@ CFLAGS=-Wall
 $(VERBOSE).SILENT:
 
 ex1: exercice1.c
-	rm -rf ex1
 	$(CC) $(CFLAGS) $^ -o $@
-	printf "\n"
-	./ex1
+
 ex2: exercice2.c
-	rm -rf ex2
 	$(CC) $(CFLAGS) $^ -o $@
-	printf "\n";
-	./ex2
+
 
 ex3: exercice3.c
-	rm -rf ex3
 	$(CC) $(CFLAGS) $^ -o $@ -lm
-	printf "\n";
-	./ex3
+
+ex6: exercice6.c
+	$(CC) $(CFLAGS) $^ -o $@
 
 clean:
 	rm -rf ex1
 	rm -rf ex2
 	rm -rf ex3
+	rm -rf ex6
---
+++ exercice6.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+char *get_field(char *line, char del, int pos) {
+    char *delim = &del;
+    char *copy = malloc(sizeof(char)*strlen(line));
+    int counter = 0;
+
+    assert(copy != NULL);
+
+    strcpy(copy, line);
+    char *token = strtok(copy, delim);
+    if(strcmp(token, "\n") == 0) {
+        // printf("*");
+        return token;
+    }
+    if(pos == counter) {
+        return token;
+    }
+    while (token != NULL) {
+        // printf(" %s\n", token);
+        token = strtok(NULL, delim);
+        if (strcmp(token, "\n") == 0) {
+            // printf("**");
+            return token;
+        }
+        counter++;
+        if(counter == pos) {
+            // printf("***");
+            return token;
+        }
+    }
+    return NULL;
+}
+
+
+int main() {
+    // FILE *flot = fopen("input.csv", "r");
+    // int i;
+    // while((i = fgetc(flot)) != EOF) {
+    //     printf("%c", i);
+    // }
+    // fclose(flot);
+    char *s = "Hello;my;name;is;Paris\n";
+    char *c = get_field(s, ';', 4);
+    printf("%s\n", c);
+}
