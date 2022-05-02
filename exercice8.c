df80cc59e8c5d3c3ee640fa809ddcbfb2293a6a9
Paris Mollo <38917755+parismollo@users.noreply.github.com>
2022-05-02 10:28:31 +0200

    Update exercice8.c

--- exercice8.c
+++ exercice8.c
@@ -7,8 +7,8 @@
 
 // [x] 1. File path comes in argument. Read arguments.
 // [x] 2. Handle number of arguments errors.
-// [ ] 3. Read each line of file and print.
-// [ ] 4. Return line number n or return number of lines.
+// [x] 3. Read each line of file and print.
+// [x] 4. Return line number n or return number of lines.
 
 int main(int argc, char **argv) {
     int n;
